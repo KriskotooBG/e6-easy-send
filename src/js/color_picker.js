91e13badfccfdab9112c4bcb437378c33b55d4c8
KriskotooBG <29774713+KriskotooBG@users.noreply.github.com>
2022-05-29 06:16:23 +0300

    Fully developed the temporary version of the Color Picker

--- src/js/color_picker.js
+++ src/js/color_picker.js
@@ -67,13 +67,16 @@ const curInteraction = {
 	colorSelBar: false,
 	alphaSelBar: false,
 
-	resetAll: () => {curInteraction.colorSelBlock = false; curInteraction.colorSelBar = false; curInteraction.alphaSelBar = false;}
+	resetAll: () => {curInteraction.colorSelBlock = false; curInteraction.colorSelBar = false; curInteraction.alphaSelBar = false;},
+	resetDefaults: () => {curInteraction.resetAll()}
 };
 const curSelPos = {
 	x: mainCanv.width - 10,
 	y: 15,
 	colorStripY: 0,
-	alphaStripY: 0
+	alphaStripY: 0,
+
+	resetDefaults: () => {curSelPos.x = mainCanv.width - 10; curSelPos.y = 15; curSelPos.colorStripY = 0, curSelPos.alphaStripY = 0}
 }
 
 const baseColor = {
@@ -81,7 +84,9 @@ const baseColor = {
 	G: 0,
 	B: 0,
 
-	setMultiple: (...values) => {if(values.length > 3) throw new TypeError("e6EasySend: Got unexpected color value length. (A1)"); let k = ["R", "G", "B"]; values.forEach((p, i) => {baseColor[k[i]] = p})}
+	setMultiple: (...values) => {if(values.length > 3) throw new TypeError("e6EasySend: Got unexpected color value length. (A1)"); let k = ["R", "G", "B"]; values.forEach((p, i) => {baseColor[k[i]] = p})},
+	resetDefaults: () => {baseColor.setMultiple(255, 0, 0)}
+	
 }
 const selectedColor = {
 	R: 0,
@@ -89,10 +94,12 @@ const selectedColor = {
 	B: 0,
 	A: 0,
 
-	setMultiple: (...values) => {if(values.length > 4) throw new TypeError("e6EasySend: Got unexpected color value length. (A2)"); let k = ["R", "G", "B", "A"]; values.forEach((p, i) => {selectedColor[k[i]] = p})}
+	setMultiple: (...values) => {if(values.length > 4) throw new TypeError("e6EasySend: Got unexpected color value length. (A2)"); let k = ["R", "G", "B", "A"]; values.forEach((p, i) => {selectedColor[k[i]] = p})},
+	resetDefaults: () => {selectedColor.setMultiple(0, 0, 0, 0)}
 }
 
-
+let isOpen = false;
+let activeSelectorBTNID = "";
 
 
 
@@ -137,7 +144,7 @@ function drawColorSelectorBox(){
 	mainCtx.clearRect(0, 0, mainCanv.width, mainCanv.height);
 
 
-	mainCtx.fillStyle = RGBAToHex(baseColor.R, baseColor.G, baseColor.B);
+	mainCtx.fillStyle = RGBAToHEX(baseColor.R, baseColor.G, baseColor.B);
 	mainCtx.fillRect(0, 0, mainCanv.width, mainCanv.height);
 
 	mainCtx.fillStyle = mainBoxLightGradient;
@@ -171,10 +178,16 @@ function drawColorSelectStrip(){
 
 
 
-function drawColorAlphaSelectStrip(){
+function drawColorAlphaSelectStrip(fromHexField = false){
 	const transpGradient = alpSelCtx.createLinearGradient(0, 0, 0, alpSelCanv.height);
-	transpGradient.addColorStop(0, RGBAToHex(selectedColor.R, selectedColor.G, selectedColor.B) + "FF");
-	transpGradient.addColorStop(1, RGBAToHex(selectedColor.R, selectedColor.G, selectedColor.B) + "00");
+	if(fromHexField){
+		transpGradient.addColorStop(0, (_hi.value.length == 7 ? _hi.value : _hi.value.slice(0, -2)) + "FF");
+		transpGradient.addColorStop(1, (_hi.value.length == 7 ? _hi.value : _hi.value.slice(0, -2)) + "00");
+	}
+	else{
+		transpGradient.addColorStop(0, RGBAToHEX(selectedColor.R, selectedColor.G, selectedColor.B) + "FF");
+		transpGradient.addColorStop(1, RGBAToHEX(selectedColor.R, selectedColor.G, selectedColor.B) + "00");
+	}
 
 	alpSelCtx.clearRect(0, 0, alpSelCanv.width, alpSelCanv.height);
 
@@ -184,7 +197,11 @@ function drawColorAlphaSelectStrip(){
 	alpSelCtx.fill();
 
 	alpSelCtx.beginPath();
-	alpSelCtx.strokeStyle = "#000000AA";
+	if(fromHexField){
+		const rgba = HEXToRGBA(_hi.value);
+		alpSelCtx.strokeStyle = (0.2126 * rgba[0] + 0.7152 * rgba[1] + 0.0722 * rgba[2]) > 128 ? "#000000AA" : "#FFFFFFAA";
+	}
+	else alpSelCtx.strokeStyle = (0.2126 * selectedColor.R + 0.7152 * selectedColor.G + 0.0722 * selectedColor.B) > 128 ? "#000000AA" : "#FFFFFFAA";
 	alpSelCtx.rect(1, curSelPos.alphaStripY - 2, alpSelCanv.width - 2, 4);
 	alpSelCtx.stroke();
 }
@@ -193,11 +210,13 @@ function drawColorAlphaSelectStrip(){
 
 
 
-function onBaseColorChange(e){
-	if(e.clientY === undefined) return;
+function onBaseColorChange(e, upd = false){
+	if(e.clientY === undefined && !upd) return;
 
-	const boundingBox = clrSelCanv.getBoundingClientRect();
-	curSelPos.colorStripY = (e.clientY - boundingBox.top) > clrSelCanv.height ? clrSelCanv.height - 1 : (e.clientY - boundingBox.top) < 0 ? 0 : (e.clientY - boundingBox.top);
+	if(!upd){
+		const boundingBox = clrSelCanv.getBoundingClientRect();
+		curSelPos.colorStripY = (e.clientY - boundingBox.top) > clrSelCanv.height ? clrSelCanv.height - 1 : (e.clientY - boundingBox.top) < 0 ? 0 : (e.clientY - boundingBox.top);
+	}
 
 	drawColorSelectStrip();
 
@@ -217,7 +236,7 @@ function onBaseColorChange(e){
 	}
 	
 
-	onBlockColorSelection();
+	if(!upd) onBlockColorSelection();
 }
 
 
@@ -282,8 +301,8 @@ function onAlphaSelection(){
 	selectedColor.A = clrData[3];
 
 	_ai.value = clrData[3];
-	_hi.value = RGBAToHex(selectedColor.R, selectedColor.G, selectedColor.B, selectedColor.A);
-	document.getElementById("popupColorPickerResultPreview").style.setProperty("--clr_picker_bgcolor", RGBAToHex(selectedColor.R, selectedColor.G, selectedColor.B, selectedColor.A))
+	_hi.value = RGBAToHEX(selectedColor.R, selectedColor.G, selectedColor.B, selectedColor.A);
+	document.getElementById("popupColorPickerResultPreview").style.setProperty("--clr_picker_bgcolor", RGBAToHEX(selectedColor.R, selectedColor.G, selectedColor.B, selectedColor.A))
 }
 
 
@@ -291,53 +310,90 @@ function onAlphaSelection(){
 
 
 function onManualColorInput(fromHex = false){
-	drawGrayOverlayOverAllCanvSelectors();
-
-
 	if(fromHex){
 		_ri.value = selectedColor.R;
 		_gi.value = selectedColor.G;
 		_bi.value = selectedColor.B;
 		_ai.value = selectedColor.A;
 	}
-	else _hi.value = RGBAToHex(selectedColor.R, selectedColor.G, selectedColor.B, selectedColor.A);
+	else _hi.value = RGBAToHEX(selectedColor.R, selectedColor.G, selectedColor.B, selectedColor.A);
 
+	temp_SetThumbsFromRGBA(selectedColor.R, selectedColor.G, selectedColor.B, selectedColor.A);
 
-	document.getElementById("popupColorPickerResultPreview").style.setProperty("--clr_picker_bgcolor", RGBAToHex(selectedColor.R, selectedColor.G, selectedColor.B, selectedColor.A))
+	document.getElementById("popupColorPickerResultPreview").style.setProperty("--clr_picker_bgcolor", RGBAToHEX(selectedColor.R, selectedColor.G, selectedColor.B, selectedColor.A));
 }
 
 
 
+//I am terribly sorry about the crimes commited in this function.
+//I am way too tired and annoyed with the 2D drawing contect API, and how innacurate It's color gradients are.
+//So, for the time being, this is the best I'll do.
+function temp_SetThumbsFromRGBA(r, g, b, a){
+	const v = RGBToHSV(r, g, b);
+	
+
+	curSelPos.x = (Math.max((Math.round(v[1] * mainCanv.width) - 1), 0));
+	curSelPos.y = mainCanv.height - Math.round(v[2] * mainCanv.height);
+	curSelPos.colorStripY = Math.round((v[0] / 360) * clrSelCanv.height);
+	curSelPos.alphaStripY = alpSelCanv.height - Math.round((a / 255) * alpSelCanv.height);
+
+	onBaseColorChange({}, true);
+	drawColorSelectorBox();
+	drawColorAlphaSelectStrip(true);
+}
 
 
 
+function openColorPickerPopup(initClr = null){
+	if(isOpen) return false;
+	
+
+	_ri.value = _gi.value = _bi.value = _ai.value = _hi.value = "0";
+
+	curInteraction.resetDefaults();
+	curSelPos.resetDefaults();
+	baseColor.resetDefaults();
+	selectedColor.resetDefaults();
 
 
-
-
-
-
-function findColorValueInBar(r,g,b, precision = 25){
-	const round = (x, p) => {return x < p ? 0 : (Math.ceil(x / p) * p)}
-	const iData = clrSelCtx.getImageData(Math.floor(clrSelCanv.width / 2), 0, 1, Math.round(clrSelCanv.height));
-
-	mainCtx.putImageData(iData, mainCanv.width / 2, 0)
-	for(let i = 0; i < iData.data.length; i += 4){
-		if(
-			round(parseInt(r), precision) == round(iData.data[i], precision) &&
-			round(parseInt(g), precision) == round(iData.data[i + 1], precision) &&
-			round(parseInt(b), precision) == round(iData.data[i + 2], precision)
-		) return i / 4;
+	if(initClr === null){
+		onBlockColorSelection();	//calling this method instead of draw, to do initial color selection.
+		drawColorSelectStrip();		//no other init methods are required as onBlockColorSelection() calls everything needed to begin.
 	}
-	return NaN;
+	else{
+		_hi.value = initClr;
+		document.getElementById("popupColorPickerResultPreview").style.setProperty("--clr_picker_bgcolor", initClr);
+		temp_SetThumbsFromRGBA(...HEXToRGBA(initClr));
+	}
+
+
+	document.getElementById("popupColorPicker_fader").style.display = "block";
+	isOpen = true;
 }
 
 
 
-function RGBAToHex(r,g,b,a){
+
+
+
+
+
+
+function RGBAToHEX(r,g,b,a){
 	return "#" + r.toString(16).padStart(2, "0") + g.toString(16).padStart(2, "0") + b.toString(16).padStart(2, "0") + (a !== undefined ? a.toString(16).padStart(2, "0") : "");
 }
-function hexToRGBA(hex){
+function CSS_RGBAToHEX(rgba){							//function from https://stackoverflow.com/a/3627747
+	return "#" + rgba.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+\.{0,1}\d*))?\)$/)
+				.slice(1)
+				.map((v, i) => {
+					return (i == 3 ? Math.round(parseFloat(v) * 255) : parseFloat(v))
+					.toString(16)
+					.padStart(2, "0")
+					.replace("NaN", "")
+				}).join("");
+	
+}
+function HEXToRGBA(hex){
 	if(hex.length != 7 && hex.length != 9) return [0, 0, 0, 0];
 
 	const x = hex.substring(1);
@@ -345,28 +401,66 @@ function hexToRGBA(hex){
 	else if(x.length == 8) return [parseInt(x[0] + x[1], 16), parseInt(x[2] + x[3], 16), parseInt(x[4] + x[5], 16), parseInt(x[6] + x[7], 16)];
 	return [0, 0, 0, 0];
 }
+function RGBToHSV(r, g, b){			//Function from https://stackoverflow.com/a/54070620
+	r /= 255, g /= 255, b/= 255;
+
+	let v = Math.max(r, g, b);
+	let c = v - Math.min(r, g, b);
+	let h = c && ((v == r) ? (g - b) / c : ((v == g) ? 2 + (b - r) / c : 4 + (r-g) / c));
+
+	return [60 * (h < 0 ? h + 6 : h), v && c / v, v];
+}
 
 
 
 
+
+
+
+
+
+
+let __mouseDownOnFader = false;
 document.getElementById("popupColorPicker_fader").addEventListener("mousedown", (e) => {
 	switch(e.target.id){
+		case "popupColorPicker_fader":
+			__mouseDownOnFader = true;
+			break;
 		case "popupColorPickerCanvMain":
+			__mouseDownOnFader = false;
 			curInteraction.colorSelBlock = true;
 			onBlockColorChange(e);
 			break;
 		case "popupColorPickerCanvColor":
+			__mouseDownOnFader = false;
 			curInteraction.colorSelBar = true;
 			onBaseColorChange(e);
 			break;
 		case "popupColorPickerCanvAlpha":
+			__mouseDownOnFader = false;
 			curInteraction.alphaSelBar = true;
 			break;
 
-		default: curInteraction.resetAll(); break;
+		default:
+			__mouseDownOnFader = false;
+			curInteraction.resetAll();
+			break;
 	}
 });
-document.getElementById("popupColorPicker_fader").addEventListener("mouseup", curInteraction.resetAll);
+document.getElementById("popupColorPicker_fader").addEventListener("mouseup", (e) => {
+	if(__mouseDownOnFader && e.target.id == "popupColorPicker_fader"){
+		document.getElementById(activeSelectorBTNID).style.backgroundColor = _hi.value;
+		document.getElementById(activeSelectorBTNID).dispatchEvent(new CustomEvent("colorupdate", {detail: _hi.value}))
+
+		document.getElementById("popupColorPicker_fader").style.display = "none";
+		isOpen = false;
+		__mouseDownOnFader = false;
+		return;
+	}
+
+	__mouseDownOnFader = false;
+	curInteraction.resetAll()
+});
 
 document.getElementById("popupColorPicker_fader").addEventListener("mousemove", (e) => {
 	if(curInteraction.colorSelBlock) onBlockColorChange(e);
@@ -380,59 +474,52 @@ document.getElementById("popupColorPicker_fader").addEventListener("mousemove", 
 
 
 
+const __onInpSetClrVal = (e, propName) => {
+	if(e.ctrlKey && (e.key == "c" || e.key == "x")) return;
+	if(!propName) throw new TypeError("propName always must be truthly!");
+	if(!["R", "G", "B", "A"].includes(propName)) throw new TypeError("propName must be on of 'R', 'G', 'B', 'A'! Got: " + propName);
 
-_ri.addEventListener("keyup", (e) => {
+
 	if(isNaN(parseInt(e.target.value))) return;
 	e.target.value = parseInt(e.target.value) > 255 ? 255 : parseInt(e.target.value) < 0 ? 0 : parseInt(e.target.value);
 	
-	selectedColor.R = parseInt(e.target.value);
+	selectedColor[propName] = parseInt(e.target.value);
 	onManualColorInput();
-});
-_gi.addEventListener("keyup", (e) => {
-	if(isNaN(parseInt(e.target.value))) return;
-	e.target.value = parseInt(e.target.value) > 255 ? 255 : parseInt(e.target.value) < 0 ? 0 : parseInt(e.target.value);
+}
+_ri.addEventListener("keyup", (e) => {__onInpSetClrVal(e, "R")});
+_gi.addEventListener("keyup", (e) => {__onInpSetClrVal(e, "G")});
+_bi.addEventListener("keyup", (e) => {__onInpSetClrVal(e, "B")});
+_ai.addEventListener("keyup", (e) => {__onInpSetClrVal(e, "A")});
 
-	selectedColor.G = parseInt(e.target.value);
-	onManualColorInput();
-});
-_bi.addEventListener("keyup", (e) => {
-	if(isNaN(parseInt(e.target.value))) return;
-	e.target.value = parseInt(e.target.value) > 255 ? 255 : parseInt(e.target.value) < 0 ? 0 : parseInt(e.target.value);
 
-	selectedColor.B = parseInt(e.target.value);
-	onManualColorInput();
+_hi.addEventListener("keydown", (e) => {
+	if(
+		(!["0", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e" ,"f",   "x", "v", "backspace", "clear", "delete", "eraseeof"].includes(e.key.toLocaleLowerCase())) ||
+		(e.target.value == "#" && ["Backspace", "Clear", "Delete", "EraseEof"].includes(e.key)) ||
+		(e.target.value.length >= 9 && !["Backspace", "Clear", "Delete", "EraseEof",].includes(e.key) && !(e.ctrlKey && ["c", "v", "a", "x"].includes(e.key.toLocaleLowerCase())))
+	) e.preventDefault();
 });
-_ai.addEventListener("keyup", (e) => {
-	if(isNaN(parseInt(e.target.value))) return;
-	e.target.value = parseInt(e.target.value) > 255 ? 255 : parseInt(e.target.value) < 0 ? 0 : parseInt(e.target.value);
-
-	selectedColor.A = parseInt(e.target.value);
-	onManualColorInput();
-});
-
 _hi.addEventListener("keyup", (e) => {
+	if(e.ctrlKey && (e.key == "c" || e.key == "x")) return;
+
 	if(!e.target.value.startsWith("#")) e.target.value = "#" + e.target.value;
 	if(e.target.value.length !== 7 && e.target.value.length !== 9) return;								//#RRGGBB(AA)
 	if(isNaN(parseInt(e.target.value.substring(1), 16))) return;
 
 
 	
-	selectedColor.setMultiple(...hexToRGBA(e.target.value.length == 7 ? e.target.value + "ff" : e.target.value));
-	onManualColorInput(true)
+	selectedColor.setMultiple(...HEXToRGBA(e.target.value.length == 7 ? e.target.value + "ff" : e.target.value));
+	onManualColorInput(true);
 });
 
 
 
 
-/*_ri.addEventListener("keyup", (e) => {
-	if(_bi.value > _gi.value) drawColorSelectStripThumb(findColorValueInBar(_ri.value, 0, _bi.value))
-	else drawColorSelectStripThumb(findColorValueInBar(_ri.value, _gi.value, 0))
-});
-_gi.addEventListener("keyup", (e) => {
-	if(_ri.value > _bi.value) drawColorSelectStripThumb(findColorValueInBar(_ri.value, _gi.value, 0))
-	else drawColorSelectStripThumb(findColorValueInBar(0, _gi.value, _bi.value))
-})
-_bi.addEventListener("keyup", (e) => {
-	if(_ri.value > _gi.value) drawColorSelectStripThumb(findColorValueInBar(_ri.value, 0, _bi.value))
-	else drawColorSelectStripThumb(findColorValueInBar(0, _gi.value, _bi.value))
-})*/
+for(const btn of document.getElementsByClassName("colorPickerButton")){
+	btn.addEventListener("click", (e) => {
+		activeSelectorBTNID = e.target.id;
+
+		const c = CSS_RGBAToHEX(e.target.style.backgroundColor);
+		openColorPickerPopup(c.length == 7 ? c + "ff" : c);
+	});
+}
